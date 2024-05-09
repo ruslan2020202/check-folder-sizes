20a92b386645236d924b4a311f032777effadc7d
ruslan2020202 <ruslan020507@gmail.com>
2024-05-09 16:39:10 +0300

    deleted docker and fix bug checkind size folders

--- size_folders_package/__main__.py
+++ size_folders_package/__main__.py
@@ -1,10 +1,15 @@
 from .size_folders import *
+import tqdm
+import time
 
 
 def main():
     try:
         try:
-            print(SizeFolder(os.getcwd()).table_info())
+            size__dir = SizeFolder(os.getcwd()).table_info()
+            for _ in tqdm.tqdm('[INFO] Loading...'):
+              time.sleep(0.1)
+            print(size__dir)
         except FileNotFoundError as e:
             print(f'[INFO] {e}')
     except KeyboardInterrupt:
